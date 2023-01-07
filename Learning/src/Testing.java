52ff2a79f296b72252679dbe688182f66aa592db
Ramakrishna <44792818+puvvadakrishna@users.noreply.github.com>
2023-01-07 20:40:18 +0000

    Initial Commit
    Initial Commit

---
+++ Learning/src/Testing.java
@@ -0,0 +1,16 @@
+
+public class Testing {
+
+	public static void main(String[] args) {
+
+		Parent pref = new Parent();
+		pref.car();
+		
+		
+		//IS-a===== 
+		//has a == 
+
+		//association
+	}
+
+}
